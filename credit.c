e053b3afc539e228624be6d6c82c48164bf6faae
matejatasic <matejatasic@users.noreply.github.com>
2023-04-29 12:39:26 +0200

    automated commit by check50 [check50=True]

---
+++ credit.c
@@ -0,0 +1,177 @@
+#include <cs50.h>
+#include <stdio.h>
+
+long SIXTEEN_DIGIT_NUMBER = 1000000000000000;
+long FIFTEEN_DIGIT_NUMBER = 100000000000000;
+long THIRTEEN_DIGIT_NUMBER = 1000000000000;
+
+string VISA = "VISA";
+string AMEX = "AMEX";
+string MASTERCARD = "MASTERCARD";
+string INVALID = "INVALID";
+
+long getDivider(long number);
+
+bool determineIsStartingDigitForMultiplication(long divider);
+
+string determineCardType(long number);
+
+int main(void)
+{
+    long number = 0;
+
+    do
+    {
+        number = get_long("Type you credit card number: ");
+    }
+    while (number < THIRTEEN_DIGIT_NUMBER);
+
+    string cardType = determineCardType(number);
+
+    if (cardType == INVALID)
+    {
+        printf("%s\n", cardType);
+
+        return 0;
+    }
+
+    long divider = getDivider(number);
+    int digit = 0;
+    bool isDigitForMultiplication = determineIsStartingDigitForMultiplication(divider);
+    int checksum = 0;
+
+    while (divider >= 1)
+    {
+        digit = (number / divider) % 10;
+
+        if (isDigitForMultiplication)
+        {
+            digit = digit * 2;
+
+            if (digit >= 10)
+            {
+                int firstDigit = digit / 10;
+                int secondDigit = digit % 10;
+
+                digit = firstDigit + secondDigit;
+            }
+
+            checksum += digit;
+        }
+        else
+        {
+            checksum += digit;
+        }
+
+        isDigitForMultiplication = !isDigitForMultiplication;
+
+        divider /= 10;
+    }
+    printf("%i\n", checksum);
+    bool isLastDigitZero = checksum % 10 == 0;
+
+    if (isLastDigitZero)
+    {
+        printf("%s\n", cardType);
+
+        return 0;
+    }
+
+    printf("%s\n", INVALID);
+}
+
+long getDivider(long number)
+{
+    long result = 0;
+
+    if (number < FIFTEEN_DIGIT_NUMBER)
+    {
+        return THIRTEEN_DIGIT_NUMBER;
+    }
+
+    else if (number < SIXTEEN_DIGIT_NUMBER)
+    {
+        return FIFTEEN_DIGIT_NUMBER;
+    }
+
+    else
+    {
+        return SIXTEEN_DIGIT_NUMBER;
+    }
+}
+
+bool determineIsStartingDigitForMultiplication(long divider)
+{
+    if (divider < FIFTEEN_DIGIT_NUMBER)
+    {
+        return true;
+    }
+
+    else if (divider < SIXTEEN_DIGIT_NUMBER)
+    {
+        return false;
+    }
+
+    else
+    {
+        return true;
+    }
+}
+
+int getStartingDigits(long number);
+
+string determineCardType(long number)
+{
+    string cardType = INVALID;
+    int startingDigits = getStartingDigits(number);
+
+    if (number < FIFTEEN_DIGIT_NUMBER)
+    {
+        if (startingDigits == 4)
+        {
+            cardType = VISA;
+        }
+    }
+
+    else if (number < SIXTEEN_DIGIT_NUMBER)
+    {
+        if (startingDigits == 34 || startingDigits == 37)
+        {
+            cardType = AMEX;
+        }
+    }
+
+    else
+    {
+        if (
+            startingDigits == 51
+            || startingDigits == 52
+            || startingDigits == 53
+            || startingDigits == 54
+            || startingDigits == 55
+        )
+        {
+            cardType = MASTERCARD;
+        }
+    }
+
+    return cardType;
+}
+
+int getStartingDigits(long number)
+{
+    if (number < FIFTEEN_DIGIT_NUMBER)
+    {
+        return number / THIRTEEN_DIGIT_NUMBER;
+    }
+
+    else if (number < SIXTEEN_DIGIT_NUMBER)
+    {
+        return number / (FIFTEEN_DIGIT_NUMBER / 10);
+    }
+
+    else
+    {
+        return number / (SIXTEEN_DIGIT_NUMBER / 10);
+    }
+}
