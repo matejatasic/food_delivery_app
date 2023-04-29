0aaf76d157c50f80364ba7a9a221f066768fa502
matejatasic <matejatasic@users.noreply.github.com>
2023-04-29 14:14:02 +0200

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -10,11 +10,13 @@ string AMEX = "AMEX";
 string MASTERCARD = "MASTERCARD";
 string INVALID = "INVALID";
 
-long getDivider(long number);
-
 bool determineIsStartingDigitForMultiplication(long divider);
 
 string determineCardType(long number);
+
+int calculateChecksum(long number);
+
+long getDivider(long number);
 
 int main(void)
 {
@@ -22,6 +24,8 @@ int main(void)
 
     number = get_long("Type your credit card number: ");
 
+    // get card type according to number length
+    // and starting digits
     string cardType = determineCardType(number);
 
     if (cardType == INVALID)
@@ -31,41 +35,12 @@ int main(void)
         return 0;
     }
 
-    long divider = getDivider(number);
-    int digit = 0;
-    bool isDigitForMultiplication = determineIsStartingDigitForMultiplication(divider);
-    int checksum = 0;
-
-    while (divider >= 1)
-    {
-        digit = (number / divider) % 10;
-
-        if (isDigitForMultiplication)
-        {
-            digit = digit * 2;
-
-            if (digit >= 10)
-            {
-                int firstDigit = digit / 10;
-                int secondDigit = digit % 10;
-
-                digit = firstDigit + secondDigit;
-            }
-
-            checksum += digit;
-        }
-        else
-        {
-            checksum += digit;
-        }
-
-        isDigitForMultiplication = !isDigitForMultiplication;
-
-        divider /= 10;
-    }
-    
+    int checksum = calculateChecksum(number);
     bool isLastDigitZero = checksum % 10 == 0;
 
+    // print the adequate card type
+    // if the last digit is zero
+    // otherwise print invalid
     if (isLastDigitZero)
     {
         printf("%s\n", cardType);
@@ -76,26 +51,7 @@ int main(void)
     printf("%s\n", INVALID);
 }
 
-long getDivider(long number)
-{
-    long result = 0;
-
-    if (number < FIFTEEN_DIGIT_NUMBER)
-    {
-        return THIRTEEN_DIGIT_NUMBER;
-    }
-
-    else if (number < SIXTEEN_DIGIT_NUMBER)
-    {
-        return FIFTEEN_DIGIT_NUMBER;
-    }
-
-    else
-    {
-        return SIXTEEN_DIGIT_NUMBER;
-    }
-}
-
+// determine if first digit is the one that will be multiplied
 bool determineIsStartingDigitForMultiplication(long divider)
 {
     if (divider < FIFTEEN_DIGIT_NUMBER)
@@ -116,6 +72,8 @@ bool determineIsStartingDigitForMultiplication(long divider)
 
 int getStartingDigits(long number);
 
+// determine card type based on the
+// length of the number and its starting digits
 string determineCardType(long number)
 {
     string cardType = INVALID;
@@ -162,6 +120,8 @@ string determineCardType(long number)
     return cardType;
 }
 
+// get the starting digits of a number
+// based on its length
 int getStartingDigits(long number)
 {
     if (number < FIFTEEN_DIGIT_NUMBER)
@@ -177,5 +137,74 @@ int getStartingDigits(long number)
     else
     {
         return number / (SIXTEEN_DIGIT_NUMBER / 10);
+    }
+}
+
+int calculateChecksum(long number)
+{
+    // get the largest divider according
+    // the length of the number
+    long divider = getDivider(number);
+    int digit = 0;
+    // check if the first digit will be
+    // the one that will be multiplied and
+    // accordingly multiply every other digit
+    // base on the first multiplied digit
+    bool isDigitForMultiplication = determineIsStartingDigitForMultiplication(divider);
+    int checksum = 0;
+
+    // get each digit and either multiply it
+    // and add its digits to the checksum
+    // or simpy add the digit to the checksum
+    while (divider >= 1)
+    {
+        digit = (number / divider) % 10;
+
+        if (isDigitForMultiplication)
+        {
+            digit = digit * 2;
+
+            if (digit >= 10)
+            {
+                int firstDigit = digit / 10;
+                int secondDigit = digit % 10;
+
+                digit = firstDigit + secondDigit;
+            }
+
+            checksum += digit;
+        }
+        else
+        {
+            checksum += digit;
+        }
+
+        isDigitForMultiplication = !isDigitForMultiplication;
+
+        divider /= 10;
+    }
+
+    return checksum;
+}
+
+// determine the divider based on the
+// length of the number
+long getDivider(long number)
+{
+    long result = 0;
+
+    if (number < FIFTEEN_DIGIT_NUMBER)
+    {
+        return THIRTEEN_DIGIT_NUMBER;
+    }
+
+    else if (number < SIXTEEN_DIGIT_NUMBER)
+    {
+        return FIFTEEN_DIGIT_NUMBER;
+    }
+
+    else
+    {
+        return SIXTEEN_DIGIT_NUMBER;
     }
 }
