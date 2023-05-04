58f46d7346c8f773f83adb0c84f9040c9183dd15
matejatasic <matejatasic@users.noreply.github.com>
2023-05-04 17:44:48 +0200

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -12,34 +12,42 @@ int main(void)
 {
     string text = get_string("Text: ");
 
+    // Get the number of letters, words and sentences
     int numberOfLetters = count_letters(text);
     int numberOfWords = count_words(text);
     int numberOfSentences = count_sentences(text);
 
+    // Calculate letters and sentences per 100 words
     float lettersPer100Words = (float)numberOfLetters / numberOfWords * 100.00;
     float sentencesPer100Words = (float)numberOfSentences / numberOfWords * 100.00;
 
+    // Calculate Coleman-Liau index
     int index = round(0.0588 * lettersPer100Words - 0.296 * sentencesPer100Words - 15.8);
 
-    if(index >= 16)
+    // Print the adequate grade according to the index
+    if (index >= 16)
     {
         printf("Grade 16+\n");
     }
-    else if(index < 1)
+    else if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-    else {
+    else
+    {
         printf("Grade %i\n", index);
     }
 }
 
+// Count the number of letters
 int count_letters(string text)
 {
     int numberOfLetters = 0;
 
     for (int i = 0, length = strlen(text); i < length; i++)
     {
+        // Increment the letter count if character is
+        // an alphanumeric
         if (isalnum(text[i]))
         {
             numberOfLetters++;
@@ -49,12 +57,15 @@ int count_letters(string text)
     return numberOfLetters;
 }
 
+// Count the number of words
 int count_words(string text)
 {
     int numberOfWords = 1;
 
     for (int i = 0, length = strlen(text); i < length; i++)
     {
+        // Increment the word count if character is
+        // an a whitespace
         if (isblank(text[i]))
         {
             numberOfWords++;
@@ -64,12 +75,15 @@ int count_words(string text)
     return numberOfWords;
 }
 
+// Count the number of sentences
 int count_sentences(string text)
 {
     int numberOfSentences = 0;
 
     for (int i = 0, length = strlen(text); i < length; i++)
     {
+        // Increment the sentence count if character is
+        // '.', '!' or a '?'
         if (
             text[i] == '.'
             || text[i] == '!'
