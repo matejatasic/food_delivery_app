9ce46c490201b8b640ba062cbdadc3b7231bb7e6
matejatasic <matejatasic@users.noreply.github.com>
2023-06-08 06:42:18 +0200

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -121,6 +121,7 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
+    printf("ok");
     for (int i = 0; i < N; i++)
     {
         node *row = table[i];
