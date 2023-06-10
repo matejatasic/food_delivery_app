5fba2a10218a05e99c111a1fcf4da8f18c39707c
matejatasic <matejatasic@users.noreply.github.com>
2023-06-10 12:38:44 +0200

    automated commit by check50 [check50=True]

---
+++ taqueria.py
@@ -0,0 +1,23 @@
+def main():
+    total = 0
+    items = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+
+    try:
+        while True:
+            item = input("Choose Item: ")
+            total += items.get(item.lower().title(), 0)
+    except EOFError:
+        print("\n")
+        print(f"Total: ${total:.2f}")
+
+main()
