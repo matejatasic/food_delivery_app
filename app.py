fb3c4af06dd6f15b05f96afd8aeb49c8315e7902
matejatasic <matejatasic@users.noreply.github.com>
2023-07-06 17:34:21 +0200

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def index():
 
     for stock in stocks:
         if not stock["symbol"] in stock_shares:
-            stock_shares[stock["symbol"]] = {"symbol": stock["symbol"], "shares": 1, "price": round(stock["price"], 2)}
+            stock_shares[stock["symbol"]] = {"symbol": stock["symbol"], "shares": 1, "price": stock["price"], "total": stock["shares"] * stock["price"]}
         else:
             stock_shares[stock["symbol"]]["shares"] += stock["shares"]
 
