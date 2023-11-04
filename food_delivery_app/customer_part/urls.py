c4091420cd4fbf58f7a7a98771d8578e9d2c652a
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2023-11-04 13:31:47 +0100

    create login and register page

--- food_delivery_app/customer_part/urls.py
+++ food_delivery_app/customer_part/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from . import views
 
 
 urlpatterns = [
-    path("", index, name="home"),
+    path("", views.index, name="home"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register")
 ]
