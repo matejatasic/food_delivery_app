6af172afca49e36012eeb86cab7c353ceb148694
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2023-11-09 17:32:11 +0100

    add cart page

--- food_delivery_app/customer_part/urls.py
+++ food_delivery_app/customer_part/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
     path("restaurant", views.restaurant, name="restaurant"),
-    path("restaurants", views.restaurants, name="restaurants")
+    path("restaurants", views.restaurants, name="restaurants"),
+    path("cart", views.cart, name="cart")
 ]
