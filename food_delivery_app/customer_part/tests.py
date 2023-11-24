8687f4127ad29cd485ada0b4747ebc4b550b90b3
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2023-11-24 08:40:41 +0100

    add login functionality

--- food_delivery_app/customer_part/tests.py
+++ food_delivery_app/customer_part/tests.py
@@ -16,9 +16,6 @@ class RegistrationTests(TestCase):
     home_url: str = reverse("home")
 
     def setUp(self) -> None:
-        # self.username = "user"
-        # self.email = "user@test.com"
-        # self.password = "password"
         self.username = "user"
         self.image_name = "img_avatar"
         self.file_image_name = f"{self.image_name}.png"
@@ -69,4 +66,42 @@ class RegistrationTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertEqual(response["location"], self.home_url)
         self.assertEqual(Profile.objects.count(), 1)
+        self.assertEqual(self.payload["username"], new_profile.user.username)
         self.assertIn(self.file_image_name, new_profile.image.name)
+
+
+class LoginTests(TestCase):
+    login_url: str = reverse("login")
+    registration_url: str = reverse("register")
+    home_url: str = reverse("home")
+
+    def setUp(self) -> None:
+        self.register_payload = register_payload = {
+            "username": "user",
+            "password1": "p@ssword123",
+            "password2": "p@ssword123",
+            "email": "user@test.com",
+            "address": "Some address",
+        }
+        self.login_payload = {
+            "username": register_payload["username"],
+            "password": register_payload["password1"],
+        }
+
+    def test_login_successful_with_valid_user(self):
+        self.client.post(self.registration_url, self.register_payload)
+        response = self.client.post(self.login_url, self.login_payload)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], self.home_url)
+        self.assertTrue(
+            User.objects.get(username=self.login_payload["username"]).is_authenticated
+        )
+
+    def test_login_with_invalid_input_returns_errors(self):
+        response = self.client.post(self.login_url, self.login_payload)
+
+        form_errors = response.context["form"].errors.as_data()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(form_errors), 1)
