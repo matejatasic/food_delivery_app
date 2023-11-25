2237dc9ecd9d05a43e27d134562f12291e792944
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2023-11-25 08:42:20 +0100

    split tests into multiple files

--- food_delivery_app/customer_part/tests/test_registration.py
+++ food_delivery_app/customer_part/tests/test_registration.py
@@ -8,14 +8,12 @@ import os
 from http import HTTPStatus
 
 from food_delivery_app.settings import MEDIA_ROOT
-from .models import Profile
-
-
-home_url: str = reverse("home")
+from ..models import Profile
 
 
 class RegistrationTests(TestCase):
     registration_url: str = reverse("register")
+    home_url: str = reverse("home")
 
     def setUp(self) -> None:
         self.username: str = "user"
@@ -56,13 +54,13 @@ class RegistrationTests(TestCase):
         response = self.client.get(self.registration_url)
 
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertEqual(response["location"], home_url)
+        self.assertEqual(response["location"], self.home_url)
 
     def test_on_registration_user_created_successfully(self) -> None:
         response = self.client.post(self.registration_url, self.payload)
 
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertEqual(response["location"], home_url)
+        self.assertEqual(response["location"], self.home_url)
         self.assertEqual(User.objects.count(), 1)
 
     def test_on_registration_profile_created_successfully(self) -> None:
@@ -71,7 +69,7 @@ class RegistrationTests(TestCase):
         new_profile: Profile = Profile.objects.get(user__username=self.username)
 
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertEqual(response["location"], home_url)
+        self.assertEqual(response["location"], self.home_url)
         self.assertEqual(Profile.objects.count(), 1)
         self.assertEqual(self.payload["username"], new_profile.user.username)
         self.assertIn(self.file_image_name, new_profile.image.name)
@@ -98,82 +96,3 @@ class RegistrationTests(TestCase):
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(len(form_errors), 2)
-
-
-class LoginTests(TestCase):
-    login_url: str = reverse("login")
-    registration_url: str = reverse("register")
-    logout_url: str = reverse("logout")
-
-    def setUp(self) -> None:
-        self.register_payload: dict[str, str] = {
-            "username": "user",
-            "password1": "p@ssword123",
-            "password2": "p@ssword123",
-            "email": "user@test.com",
-            "address": "Some address",
-        }
-        self.login_payload: dict[str, str] = {
-            "username": self.register_payload["username"],
-            "password": self.register_payload["password1"],
-        }
-
-    def test_access_to_login_as_authenticated_user_unsuccessful(self):
-        self.client.post(self.registration_url, self.register_payload)
-        response = self.client.get(self.login_url)
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertEqual(response["location"], home_url)
-
-    def test_login_successful_with_valid_user(self) -> None:
-        self.client.post(self.registration_url, self.register_payload)
-        self.client.post(self.logout_url)
-        response = self.client.post(self.login_url, self.login_payload)
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertEqual(response["location"], home_url)
-        self.assertTrue(
-            User.objects.get(username=self.login_payload["username"]).is_authenticated
-        )
-
-    def test_login_with_invalid_input_returns_errors(self) -> None:
-        response = self.client.post(self.login_url, self.login_payload)
-
-        form_errors: dict[str, list[ValidationError]] = response.context[
-            "form"
-        ].errors.as_data()
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertEqual(len(form_errors), 1)
-
-
-class LogoutTests(TestCase):
-    logout_url: str = reverse("logout")
-    login_url: str = reverse("login")
-    registration_url: str = reverse("register")
-
-    def test_logout_successful(self) -> None:
-        register_payload: dict[str, str] = {
-            "username": "user",
-            "password1": "p@ssword123",
-            "password2": "p@ssword123",
-            "email": "user@test.com",
-            "address": "Some address",
-        }
-        login_payload: dict[str, str] = {
-            "username": register_payload["username"],
-            "password": register_payload["password1"],
-        }
-
-        self.client.post(self.registration_url, register_payload)
-        self.client.post(self.login_url, login_payload)
-        response = self.client.post(self.logout_url)
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertEqual(response["location"], home_url)
-
-    def test_logout_unsuccessful_when_user_anonymous(self) -> None:
-        response = self.client.post(self.logout_url)
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertIn(self.login_url, response["location"])
