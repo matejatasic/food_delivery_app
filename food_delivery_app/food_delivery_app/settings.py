e9e5aa9de467cf8feaff0485b189eae740ae319a
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2023-12-15 13:00:25 +0100

    configure logging

--- food_delivery_app/food_delivery_app/settings.py
+++ food_delivery_app/food_delivery_app/settings.py
@@ -114,6 +114,36 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DJANGO_ERROR_LOGGER = "django_error"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module}:\n{message}\n{pathname}, line {lineno}, in {funcName}\n",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": f"{BASE_DIR}/error.log",
+            "formatter": "verbose"
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        DJANGO_ERROR_LOGGER: {
+            "handlers": ["file", "console"],
+            "level": "ERROR",
+            "propagate": True
+        }
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
