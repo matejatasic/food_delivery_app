87593e56ca5dacb42c86e3db23965ea311d95d10
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2023-12-30 15:22:24 +0100

    write unit tests for restaurant service

--- food_delivery_app/customer_part/services/restaurant_service.py
+++ food_delivery_app/customer_part/services/restaurant_service.py
@@ -7,6 +7,9 @@ from ..models import Restaurant, RestaurantCategory, RestaurantLike
 
 
 class RestaurantService:
+    LIKED = "liked"
+    UNLIKED = "unliked"
+
     def get_all(self) -> list[dict[str, str | int]]:
         return [
             RestaurantDto(
@@ -23,26 +26,41 @@ class RestaurantService:
         return RestaurantCategory.objects.all()
 
     def like(self, restaurant_id: str, authenticated_user: User) -> tuple[str, int]:
-        if not Restaurant.objects.filter(id=restaurant_id).exists():
+        if not self.restaurant_exists(id=restaurant_id):
             raise RestaurantDoesNotExist()
 
-        restaurant = Restaurant.objects.get(id=restaurant_id)
+        restaurant = self.get_by_id(id=restaurant_id)
+
         try:
-            like = RestaurantLike.objects.get(
-                restaurant__id=restaurant_id, user=authenticated_user
+            like = self.get_like(
+                restaurant_id=restaurant_id, authenticated_user=authenticated_user
             )
             like.delete()
             action = "unliked"
         except:
-            RestaurantLike.objects.create(
-                restaurant=restaurant, user=authenticated_user
+            self.create_like(
+                restaurant=restaurant, authenticated_user=authenticated_user
             )
             action = "liked"
 
         return (action, restaurant.likes.count())
 
-    def get_by_category(self, category_name: str | None):
-        if not RestaurantCategory.objects.filter(name=category_name).exists():
+    def get_by_id(self, id: str) -> Restaurant:
+        return Restaurant.objects.get(id=id)
+
+    def restaurant_exists(self, id: str) -> bool:
+        return Restaurant.objects.filter(id=id).exists()
+
+    def get_like(self, restaurant_id: str, authenticated_user: User) -> RestaurantLike:
+        return RestaurantLike.objects.get(
+            restaurant__id=restaurant_id, user=authenticated_user
+        )
+
+    def create_like(self, restaurant: Restaurant, authenticated_user: User) -> None:
+        RestaurantLike.objects.create(restaurant=restaurant, user=authenticated_user)
+
+    def get_by_category(self, category_name: str | None) -> list[dict[str, str | int]]:
+        if not self.category_exists(category_name=category_name):
             raise RestaurantCategoryDoesNotExist()
 
         return [
@@ -53,5 +71,15 @@ class RestaurantService:
                 image=restaurant.image.name,
                 number_of_likes=restaurant.likes.count(),
             ).toDict()
-            for restaurant in Restaurant.objects.filter(category__name=category_name)
+            for restaurant in self.get_restaurants_by_category_queryset(
+                category_name=category_name
+            )
         ]
+
+    def category_exists(self, category_name: str | None) -> bool:
+        return RestaurantCategory.objects.filter(name=category_name).exists()
+
+    def get_restaurants_by_category_queryset(
+        self, category_name: str | None
+    ) -> QuerySet[Restaurant]:
+        return Restaurant.objects.filter(category__name=category_name)
